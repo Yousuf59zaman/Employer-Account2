ecb4956f2c7bd56b5941205c80505f16da1333c8
irin <irin@IT-IRIN>
2025-01-05 17:52:04 +0600

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ErrorModalComponent } from './components/error-modal/error-modal.compon
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,ErrorModalComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
