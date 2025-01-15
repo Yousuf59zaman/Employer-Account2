45502edb66c78e4725558b06a94cd6f132f2c7dc
irin <irin@IT-IRIN>
2025-01-15 17:46:57 +0600

    bottom-footer added

--- src/app/layouts/footer-bottom/footer-bottom.component.ts
+++ src/app/layouts/footer-bottom/footer-bottom.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer-bottom',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer-bottom.component.html',
   styleUrl: './footer-bottom.component.scss'
 })
 export class FooterBottomComponent {
+  dropdownOpen: boolean = false;
+  dropdownOpenInternational: boolean = false;
 
+  toggleDropdown(): void {
+    this.dropdownOpenInternational = false;
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  toggleDropdown2(): void {
+    this.dropdownOpen = false;
+    this.dropdownOpenInternational = !this.dropdownOpenInternational;
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  closeDropdown(targetElement: HTMLElement): void {
+    const clickedInside = targetElement.closest('.relative');
+    if (!clickedInside) {
+      this.dropdownOpen = false;
+      this.dropdownOpenInternational = false;
+    }
+  }
 }
