144bbb2d064550f11397cac8cd7acf1dfcfbc4ba
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-06-22 11:51:06 +0600

    rl no modal issue and navbar issue solved

--- src/app/components/error-modal/error-modal.component.ts
+++ src/app/components/error-modal/error-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-error-modal',
@@ -6,11 +6,20 @@ import { Component, Output, EventEmitter } from '@angular/core';
   templateUrl: './error-modal.component.html',
   styleUrls: ['./error-modal.component.scss']
 })
-export class ErrorModalComponent {
+export class ErrorModalComponent implements OnInit, OnDestroy {
 
   @Output() modalClosed = new EventEmitter<void>();
 
+  ngOnInit(): void {
+    document.body.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy(): void {
+    document.body.style.overflow = 'auto';
+  }
+
   closeModal(): void {
+    document.body.style.overflow = 'auto';
     this.modalClosed.emit();
   }
 }
